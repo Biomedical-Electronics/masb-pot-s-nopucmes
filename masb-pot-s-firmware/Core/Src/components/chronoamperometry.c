64f045140dcd39ee964bf83b609f288257405ef0
Francisco-Domingo-Gil <fran.dg12@outlook.com>
2021-05-13 19:51:50 +0200

    Creacion archivos

--- masb-pot-s-firmware/Core/Src/components/chronoamperometry.c
+++ masb-pot-s-firmware/Core/Src/components/chronoamperometry.c
@@ -6,5 +6,9 @@
   * @copyright	Copyright 2020 Albert Álvarez Carulla. All rights reserved.
   ******************************************************************************
   */
-
+#define MCP4725_ID 0x60
 #include "components/chronoamperometry.h"
+void chronoamp(struct CA_Configuration_S caConfiguration){
+	double eDC=caConfiguration.eDC; //leemos edc del parámetro de entrada
+	Voltage2Dac(eDC); //
+}
