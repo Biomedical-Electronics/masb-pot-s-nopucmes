426248e589a47a3cf1ee607794577ff72f52f49f
Francisco-Domingo-Gil <fran.dg12@outlook.com>
2021-05-16 14:01:43 +0200

    Añadir include stm32main.h

--- masb-pot-s-firmware/Core/Src/main.c
+++ masb-pot-s-firmware/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "components/stm32main.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
