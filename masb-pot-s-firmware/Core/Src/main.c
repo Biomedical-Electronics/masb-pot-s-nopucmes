50e0d91dac3648c826b0e77928b3ae0a37898d31
Adrià Díaz Ferrero <adriadiazferrero@gmail.com>
2021-05-16 13:17:07 +0200

    Modificación para probar stm32main.

--- masb-pot-s-firmware/Core/Src/main.c
+++ masb-pot-s-firmware/Core/Src/main.c
@@ -102,12 +102,18 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
+  struct Handles_S myHandles;
+  myHandles.huart = &huart2;
+
+  setup(&myHandles);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  loop();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
