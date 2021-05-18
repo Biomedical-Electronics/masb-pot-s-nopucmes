137266c4cdccf5b9fe5ca19517c978538e0c0a11
Lluis MASB <llcolomc66@alumnes.ub.edu>
2021-05-18 14:38:19 +0200

    creacio cyclic voltammetry

--- masb-pot-s-firmware/Core/Src/components/cyclic_voltammetry.c
+++ masb-pot-s-firmware/Core/Src/components/cyclic_voltammetry.c
@@ -8,3 +8,73 @@
   */
 
 #include "components/cyclic_voltammetry.h"
+
+
+
+extern TIM_HandleTypeDef htim2;
+
+void Voltammetry_Config(struct CV_Configuration_S cvConfiguration){
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);                // Cerramos rele (EN - HIGH (1))
+
+	float vdac = (float)(1.65-(cvConfiguration.eBegin/2.0));   // Formula 1
+
+	MCP4725_SetOutputVoltage(hdac, vdac);   // Fijamos el valor de Vcell a eBegin
+
+	// Igualamos VObjetivo a eVertex1
+
+	// double Vobj = cvConfiguration.eVertex1;
+
+	__HAL_TIM_SET_AUTORELOAD(&htim2, cvConfiguration.eStep/cvConfiguration.scanRate); // sampling period defined by eStep/scanRate
+
+	HAL_TIM_Base_Start_IT(&htim2);            // Iniciamos el timer
+
+}
+
+void Voltammetry_Value(struct CV_Configuration_S cvConfiguration){
+
+	uint32_t cycles = 0;
+
+	_Bool measure = FALSE;
+
+	double vcell=cvConfiguration.eBegin;
+
+	double vobj=cvConfiguration.eVertex1;
+
+	while(cycles <= cvConfiguration.cycles){
+
+		if (measure){
+			if (vcell==vobj){
+
+			} else {
+
+				if (vcell+cvConfiguration.eStep>vobj){
+
+
+
+				} else {
+
+					vcell+=((vobj-vcell)/abs(vobj-vcell))*cvConfiguration.eStep; // incrementem eStep a Vcell
+
+					float vdac = (float)(1.65-(vcell/2.0)); // definim el vdac a partir del Vcell que volem donar
+
+					MCP4725_SetOutputVoltage(hdac, vdac); // administrem el nou voltatge al Working Electrode
+
+				}
+
+			}
+
+		} else {
+
+		}
+
+
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);  // Abrimos rele (EN - LOW (0))
+
+	__HAL_TIM_SET_COUNTER(&htim2, 0);         // Reiniciamos el contador del timer a 0
+
+	HAL_TIM_Base_Stop_IT(&htim2);             // Detenemos el timer al finalizar la medición
+
+
+}
